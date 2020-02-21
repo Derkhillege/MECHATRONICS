0460aefb8ab17539672a370a8cc683bc3d225573
Derk <derkhillege@gmail.com>
2020-02-21 17:17:49 +0100

    Last Commit of the day! In model driver you can set the starting parameters of the SImulink model.

--- Model_driver.m
+++ Model_driver.m
@@ -20,35 +20,21 @@
 clc; clear all; close all;
 
 % load properties and parameters
-    get_parameters;
 
-% simulation control parameters
-    t_end        = 36000;
-    iv_t_control = [   0,  (1/10)*t_end, (2/10)*t_end,  (5/10)*t_end,  (6/10)*t_end, (7/10)*t_end, t_end];         % [s]
-    ov_X_set     = [ 100,           100,           30,            40,            40,           90,    90]/100;     % [%] of maximum fuelrack
-    ov_Y_set     = [ 100,           100,          100,           100,           100,          100,   100]/100;     % [%] disturbance factor
+
+
+% SB MOTOR PID Set Values
+SB_I = 10
+SB_P = 10
+SB_D = 10
+%Ki_x = 1
     
-    % experiment 1
-%     t_end        = 1500;
-%     iv_t_control = [   0,  (1/5)*t_end, (1/5)*t_end, (2/5)*t_end, (2/5)*t_end, (3/5)*t_end, (3/5)*t_end, (4/5)*t_end, (4/5)*t_end, t_end];         % [s]
-%     ov_X_set     = [ 100,          100,          75,          75,          50,          50,          25,          25,           1,     1]/100;     % [%] of maximum fuelrack
-%     ov_Y_set     = [ 100,          100,         100,         100,         100,         100,         100,         100,         100,   100]/100;     % [%] disturbance factor
 
-% Initial values
-    in_eng       = n_eng_nom/i_gb;
-    iv_s         = 7.54;
 
 %% Simulation
 
-open([cd,'/models/propulsion.mdl']);
-
-tic
-sim('propulsion',t_end);
-toc
-
-%% Post-processing
-plot_file;
-%% Modifications:
-% 121012: Created                                (PdV)
-% 140127: Inserted experiment 1                  (PdV)
-% 140205: Final version for MT1452               (PdV)
+% open([cd,'/wetmodel.slx']);
+% 
+% tic
+% sim('wetmodel',t_end);
+% toc
