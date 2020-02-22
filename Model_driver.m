bea085c5710c1fc8e0bc804e65012cb9488e1101
Kirsten <odendaal_k@yahoo.ca>
2020-02-22 12:31:46 +0100

    Added displays for better process visualization

--- Model_driver.m
+++ Model_driver.m
@@ -21,25 +21,34 @@ clc; clear all; close all;
 
 % load properties and parameters
 
-%PID Controller Tuning Parameters
+% PID Controller Tuning Parameters
 
-Kp_x = 10
-Ki_x = 1
-Kd_x = 0
+Kp_x = 10;
+Ki_x = 0;
+Kd_x = 0;
 
-Kp_y = 10
-Ki_y = 0
-Kd_y = 0
+Kp_y = 10;
+Ki_y = 0;
+Kd_y = 0;
 
-Kp_yaw = 10
-Ki_yaw = 0
-Kd_yaw = 0
+Kp_yaw = 10;
+Ki_yaw = 0;
+Kd_yaw = 0;
 
+% Thrust Allocation Settings - Configuration 5 (Angle,Xposition,Yposition)
+% Starboard Thruster
+SB_thruster = [0 -0.05 0.05];
+
+% Port Thruster
+P_thruster = [pi/2 -0.05 -0.05]; %need to determine where angle is +/-
+
+% Bow Thruster
+B_thruster = [pi/2 0.05 0];
 
 % SB MOTOR PID Set Values
-SB_I = 10
-SB_P = 10
-SB_D = 10
+SB_I = 10;
+SB_P = 10;
+SB_D = 10;
 
     
 
